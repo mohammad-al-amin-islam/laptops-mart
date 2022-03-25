65bff9a9684c9360d8a4de19ca594090cb131dc1
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-25 23:28:10 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-area'>
+            <h1>Attic's Mart</h1>
+            <h3>Choose Your Desired Laptop</h3>
+        </div>
+    );
+};
+
+export default Header;
